38c7bca72ab480af780720e2651b2079dda93710
PawanCvakoti <shivakotipawan8@gmail.com>
2021-05-25 21:39:03 +0545

    javascript used to make hamburger icon functional

---
+++ script.js
@@ -0,0 +1,5 @@
+/*For Desining Hamburger Icon*/
+function toggleClass(){
+	let menu = document.querySelector(".list");
+	menu.classList.toggle("toggleCls");
+document.getElementById("menu1").classList.toggle("change");}
