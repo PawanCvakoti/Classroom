3a05262bb0c792c14a70b2b7c317819ba08a8fd2
PawanCvakoti <shivakotipawan8@gmail.com>
2021-05-26 21:29:51 +0545

    added references and adjusted divs

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 /*For Desining Hamburger Icon*/
-function toggleClass(){
+function Converter(){
 	let menu = document.querySelector(".list");
-	menu.classList.toggle("toggleCls");
+	menu.classList.toggle("change1");
 document.getElementById("menu1").classList.toggle("change");}
+/*TubeMint(2019)How to create hamburger menu With HTML,CSS,&JAVASCRIPT[Online].
+Available from:https://www.youtube.com/watch?v=8r3hGIvyP3k[Accessed 25 May 2021]*/
+
+ /*Hrishi Computer Education(2020)Pure CSS Hamburger Menu|CSS Beginner's Guide in Hindi
+[Online].Available from:https://www.youtube.com/watch?v=2jtnMjPVxvI[Accessed 25 May 2021]*/
