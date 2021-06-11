deec534a8ca95dfed2e5bd0e455543ca6e74bfe0
PawanCvakoti <shivakotipawan8@gmail.com>
2021-06-11 12:59:08 +0545

    modified inline citation and reference

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-/*For Desining Hamburger Icon*/
+/*For Making Hamburger Icon Functional*/
 function Converter(){
 	let menu = document.querySelector(".list");/*(TubeMint, 2019)*/
 	menu.classList.toggle("change1");/*(TubeMint, 2019)*/
     document.getElementById("menu1").classList.toggle("change");}/*(Hrishi Computer Education, 2020)*/
 
-/*TubeMint(2019)How to create hamburger menu With HTML,CSS,&JAVASCRIPT. YouTube[Online].
+/*TubeMint (2019) How to create hamburger menu With HTML,CSS,&JAVASCRIPT. YouTube[Online].
 Available from:https://www.youtube.com/watch?v=8r3hGIvyP3k[Accessed 25 May 2021]*/
 
-/*Hrishi Computer Education(2020)Pure CSS Hamburger Menu|CSS Beginner's Guide in Hindi. YouTube[Online]. 
+/*Hrishi Computer Education (2020) Pure CSS Hamburger Menu|CSS Beginner's Guide in Hindi. YouTube[Online]. 
 Available from:https://www.youtube.com/watch?v=2jtnMjPVxvI[Accessed 25 May 2021]*/
