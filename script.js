3d883b7b2ba88bd6c9bdc01be752652f60396091
PawanCvakoti <shivakotipawan8@gmail.com>
2021-06-19 22:12:09 +0545

    modified codes used to make hamburger icon functional

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
 /*For Making Hamburger Icon Functional*/
 function Converter(){
-	let menu = document.querySelector(".list");/*(TubeMint, 2019)*/
-	menu.classList.toggle("change1");/*(TubeMint, 2019)*/
-    document.getElementById("menu1").classList.toggle("change");}/*(Hrishi Computer Education, 2020)*/
+    document.getElementById('list').classList.toggle('cross');
+}/*(Code Spot, 2018)*/
 
-/*TubeMint (2019) How to create hamburger menu With HTML,CSS,&JAVASCRIPT. YouTube[Online].
-Available from:https://www.youtube.com/watch?v=8r3hGIvyP3k[Accessed 25 May 2021]*/
-
-/*Hrishi Computer Education (2020) Pure CSS Hamburger Menu|CSS Beginner's Guide in Hindi. YouTube[Online]. 
-Available from:https://www.youtube.com/watch?v=2jtnMjPVxvI[Accessed 25 May 2021]*/
+/*Code Spot (2018) Easy Hamburger Menu Animation using HTML, CSS [For Beginners]. YouTube[Online]. 
+Available from:https://www.youtube.com/watch?v=F-mvXKKoSCU[Accessed 19 JUN 2021]*/
