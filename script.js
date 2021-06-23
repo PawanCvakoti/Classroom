92fa5346e79dee6138d2f7d90b709f75e4ea276e
PawanCvakoti <shivakotipawan8@gmail.com>
2021-06-23 15:08:02 +0545

    deleted javascript codes used to make hamburger menu

--- script.js
+++
@@ -1,7 +0,0 @@
-/*For Making Hamburger Icon Functional*/
-function Converter(){
-    document.getElementById('list').classList.toggle('cross');
-}/*(Code Spot, 2018)*/
-
-/*Code Spot (2018) Easy Hamburger Menu Animation using HTML, CSS [For Beginners]. YouTube[Online]. 
-Available from:https://www.youtube.com/watch?v=F-mvXKKoSCU[Accessed 19 JUN 2021]*/
